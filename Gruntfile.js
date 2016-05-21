d1ace06135db5fbc0bafaa9149f05c84a28030b2
Chris Skaaning <Chris Skaaning>
2016-05-21 19:08:11 +0100

    Added travis build info and tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,11 +21,25 @@ module.exports = function(grunt) {
 		      "public/css/main.css": ["node_modules/bootstrap/dist/css/bootstrap.css", "src/less/**/*.less"]
 		    }
 	    },
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          captureFile: 'results.txt', // Optionally capture the reporter output to a file 
+          quiet: false, // Optionally suppress output to standard out (defaults to false) 
+          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false) 
+        },
+        src: ['test/**/*.js']
+      }
     }
   });
 
   require('load-grunt-tasks')(grunt);
 
+  grunt.registerTask('build', ['mochaTest', 'browserify'])
+
+  grunt.registerTask('test', ['mochaTest']);
   // Default task(s).
   grunt.registerTask('default', ['browserify', 'less:dist', 'nodemon:dist']);
 
